ba2e6ca909c6b50872fd136246fb7e7609223df8
Vlad <numaeria@gmail.com>
2026-05-16 19:13:56 +0300

    пофиксили крэш

---
+++ Image Feed/ImagesList/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  Image Feed
+//
+//  Created by Влад Скрипченко on 15.05.26.
+//
+
+import UIKit
+
+final class SingleImageViewController {
+    @IBOutlet var imageView: UIImageView!
+}
