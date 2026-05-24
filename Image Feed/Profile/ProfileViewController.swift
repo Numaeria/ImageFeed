e182e874f0dbd109a897c9b4016b5692616a1aae
Vlad <numaeria@gmail.com>
2026-05-24 17:04:44 +0300

    сверстал вручную экран профиля + добавил логику, что при нажатии на кнопку didTapLogoutButton меняется аватарка и исчезает вся информация из профиля

--- Image Feed/Profile/ProfileViewController.swift
+++ Image Feed/Profile/ProfileViewController.swift
@@ -9,10 +9,94 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var leaveProfileButton: UIButton!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-    @IBAction private func didTapLogoutButton() {}
+    private var avatarImageView: UIImageView!
+    private var leaveProfileButton: UIButton!
+    private var nameLabel: UILabel!
+    private var loginNameLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        avatarImageView = UIImageView()
+        avatarImageView.image = UIImage(named: "322")
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(avatarImageView)
+        
+        avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        avatarImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        avatarImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        nameLabel = UILabel()
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8).isActive = true
+        
+        loginNameLabel = UILabel()
+        loginNameLabel.textColor = UIColor(hex: "#AEAFB4")
+        loginNameLabel.font = UIFont.systemFont(ofSize: 13)
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginNameLabel)
+        
+        loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        descriptionLabel = UILabel()
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.text = "Hello,world!"
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        descriptionLabel.leadingAnchor.constraint(equalTo: loginNameLabel.leadingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        leaveProfileButton = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(didTapLogoutButton)
+        )
+        leaveProfileButton.tintColor = UIColor(hex: "#F56B6C")
+        leaveProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(leaveProfileButton)
+        
+        leaveProfileButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        leaveProfileButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor).isActive = true
+    }
+    
+    @objc
+    private func didTapLogoutButton() {
+        nameLabel.removeFromSuperview()
+        loginNameLabel.removeFromSuperview()
+        descriptionLabel.removeFromSuperview()
+        
+        avatarImageView.image = UIImage(systemName: "person.crop.circle.fill")
+        avatarImageView.tintColor = .gray
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        var cleanHex = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if cleanHex.hasPrefix("#") {
+            cleanHex.remove(at: cleanHex.startIndex)
+        }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: cleanHex).scanHexInt64(&rgbValue)
+        
+        let r = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgbValue & 0x0000FF) / 255.0
+        
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
 }
